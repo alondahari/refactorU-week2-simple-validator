18e28786e81bb7bf47138ff1266ff537f7719705
Alon Dahari <climbinghobo@gmail.com>
2015-01-19 15:30:00 -0700

    add basic phone validation functionality

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-// alert( ( (phoneNumber.charAt(3) == '-') && (phoneNumber.charAt(7) == '-') ? 'valid' : 'invalid' ) + ' phone number');
-
 var button = document.getElementById('check-valid');
 
-button.onclick = function (){
+function validatePhone() {
+  var p = document.getElementById('feedback-phone');
+  phoneNumber = document.getElementById('phone-number');
+  phoneNumber = phoneNumber.value;
+  var feedback = (phoneNumber.charAt(3) == '-') && (phoneNumber.charAt(7) == '-') ? 'valid' : 'invalid' ;
+  p.textContent = p.className = feedback;
+}
 
+button.onclick = function (){
+  validatePhone();
 }
