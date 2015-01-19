d0b811bf02212e3b8edc951b96016cb75ef3fc1d
Alon Dahari <climbinghobo@gmail.com>
2015-01-19 16:13:15 -0700

    structure js

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 var button = document.getElementById('check-valid');
 
-function validatePhone() {
-  var p = document.getElementById('feedback-phone');
-  phoneNumber = document.getElementById('phone-number');
-  phoneNumber = phoneNumber.value;
-  var feedback = (phoneNumber.charAt(3) == '-') && (phoneNumber.charAt(7) == '-') ? 'valid' : 'invalid' ;
-  p.textContent = p.className = feedback;
+var fields = {
+  "phone" : function(value){
+    return (value.charAt(3) == '-' && value.charAt(7) == '-');
+  },
+  "birth-date" : function(value){
+    return (value.charAt(3) == '-' && value.charAt(7) == '-');
+  },
+  "postal-code" : function(value){
+    return (value.charAt(3) == '-' && value.charAt(7) == '-');
+  },
+  "state" : function(value){
+    return (value.charAt(3) == '-' && value.charAt(7) == '-');
+  },
+  "married" : function(value){
+    return (value.charAt(3) == '-' && value.charAt(7) == '-');
+  }
+};
+
+function validate(elId, validator) {
+  var value = document.getElementById(elId).value;
+  return validator(value);
+}
+
+function feedback(elId, value) {
+  var p = document.getElementById('feedback-' + elId);
+  p.textContent = p.className = value ? 'valid' : 'invalid';
 }
 
 button.onclick = function (){
-  validatePhone();
-}
+  var field, isValid;
+  for (field in fields) {
+    isValid = validate( field, fields[field] );
+    feedback(field, isValid);
+  }
+
+};
